f3b8d1118ad1f52f4d0323d1aaa9a80349dc6e89
Andrea_Graziosi <andreaggraziosi@gmail.com>
2021-07-06 16:52:48 -0700

    imports

--- app/extensions.py
+++ app/extensions.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_pymongo import PyMongo
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-#from app.config import Config
+from app.config import Config
 import os
 
 app = Flask(__name__)
