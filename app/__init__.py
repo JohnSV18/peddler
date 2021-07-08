615a0d9765a72141fbfa6107279dcdc3245e6433
Aetrix27 <davedogg323@gmail.com>
2021-07-08 12:00:42 -0700

    fix bugs

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,7 @@
 from app.main.routes import main
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-#from events_app.config import Config
 import os
 
 app = Flask(__name__)
-# app.config.from_object(Config)
-app.secret_key = os.urandom(24)
 
-#db = SQLAlchemy(app)
-
-
-# app.register_blueprint(main)
-
-# with app.app_context():
-# db.create_all()
+app.register_blueprint(main)
