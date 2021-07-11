74526fad7f9fd6caa31a74ba635647c30de60e46
JohnSV18 <69915500+JohnSV18@users.noreply.github.com>
2021-07-11 10:04:40 -0700

    Cleaning up files

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from app.auth.auth_routes import auth
 app.register_blueprint(main)
 app.register_blueprint(auth)
 
-# with app.app_context():
-#    db.create_all()
 
 if (__name__ == "__main__"):
     app.run(debug=True)
